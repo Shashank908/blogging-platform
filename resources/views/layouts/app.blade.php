e3c49c02f1a286229fe871e8d2276a72d341621f
Shashank Shekhar <ershashankshekhar00@gmail.com>
2021-02-05 17:21:12 +0530

    sort by publishing date option

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
